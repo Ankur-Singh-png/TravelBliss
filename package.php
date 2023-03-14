878093b7392bb972c0dfede617cf7f0271cb615b
Indominous <93007035+Indominous1@users.noreply.github.com>
2023-03-15 02:03:23 +0530

    Add files via upload

---
+++ package.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Packages</title>
+
+    <!-- custom swiper file link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!-- header starts -->
+<section class="header">
+    <a href="home.php" class="logo">TravelBliss</a>
+
+    <nav class="navbar">
+        <a href="home.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="package.php">package</a>
+        <a href="book.php">Book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+<!-- header ends -->
+
+<div class="heading" style="background:url(.//images/package_image.jpg) ">
+   <h1>Packages</h1>
+</div>
+
+<!-- package section starts -->
+<section class="packages">
+
+    <h1 class="heading-title">Top Destinations</h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+              <img src=".//images/Badrinath_Temple_,_Uttarakhand.jpg" alt="">  
+            </div>
+
+            <div class="content">
+                <h3>Badrinath Temple</h3>
+                <p>Badrinath or Badrinarayana Temple is a Hindu temple dedicated to Vishnu. It is situated in the town of Badrinath in Uttarakhand, India.It is one of the Char Dham pilgrimage sites.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+              <img src=".//images/Dwarkadhish-Temple-Gujarat.jpg" alt="">  
+            </div>
+
+            <div class="content">
+                <h3>Dwarkadhish Temple</h3>
+                <p>The Dwarkadhish temple or Jagat Mandir and occasionally spelled Dwarakadheesh, is a Hindu temple dedicated to Krishna he temple is located at Dwarka city of Gujarat, India</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+              <img src=".//images/jagannath temple.jpg" alt="">  
+            </div>
+
+            <div class="content">
+                <h3>Jagannath Temple</h3>
+                <p>The Jagannath Temple is an important Hindu temple dedicated to Jagannath, a form of Vishnu. It is situated in the Puri in Orissa, India.It is one of the Char Dham pilgrimage sites.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+              <img src=".//images/rameshwaram-temple-1.jpg" alt="">  
+            </div>
+
+            <div class="content">
+                <h3>Rameshwaram Temple</h3>
+                <p>Ramanathaswamy Temple is a Hindu temple dedicated to the god Shiva located on Rameswaram island in the state of Tamil Nadu, India. It is also one of the twelve Jyotirlinga temples</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+              <img src=".//images/Kedarnath-yatra-Package.jpg" alt="">  
+            </div>
+
+            <div class="content">
+                <h3>Kedarnath Temple</h3>
+                <p>Kedarnath Temple is a Hindu temple roughly 1200 years old, dedicated to Shiva. The temple is located on the Garhwal Himalayan range near the Mandakini river, in the state of Uttarakhand, India and is one of the four sites in the Chota Char Dham pilgrimage circuit</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+              <img src=".//images/Gangotri-Dham-Temple.jpg" alt="">  
+            </div>
+
+            <div class="content">
+                <h3>Gangotri Temple</h3>
+                <p>A temple dedicated to Goddess Ganga is located near a sacred stone where King Bhagiratha worshipped Lord Shiva.Gangotri is one of the four sites in the Chota Char Dham pilgrimage circuit.It is situated in Uttarkashi district in the state of Uttarakhand, India</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+              <img src=".//images/yamunori-temple.jpg" alt="">  
+            </div>
+
+            <div class="content">
+                <h3>Yamunotri Temple</h3>
+                <p>Yamunotri temple has a shrine dedicated to Goddess Yamuna, and has a black marble idol of the goddess. It is situated in the western region of Garhwal Himalayas in Uttarkashi district, Uttarakhand, India.Yamunotri temple is part of the Char Dham pilgrimage circuit.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+              <img src=".//images/kashi-vishvnath temple.jpg" alt="">  
+            </div>
+
+            <div class="content">
+                <h3>kashi vishvnath temple</h3>
+                <p>Kashi Vishwanath Temple is a famous Hindu temple dedicated to Lord Shiva. It is located in Vishwanath Gali of Varanasi, Uttar Pradesh in India. It is also one of the twelve Jyotirlinga temples and it stands on the western bank of the holy river Ganga.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+              <img src=".//images/prem-mandir-vrindavan.jpg" alt="">  
+            </div>
+
+            <div class="content">
+                <h3>prem mandir</h3>
+                <p>Prem Mandir is a temple in Vrindavan, Mathura district, Uttar Pradesh, India. It is near the holy city of Mathura. It is maintained by Jagadguru Kripalu Parishat, an international non-profit, educational, spiritual, charitable trust.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+              <img src=".//images/krishna-balram temple.jpg" alt="">  
+            </div>
+
+            <div class="content">
+                <h3>krishna balram temple</h3>
+                <p>Sri Sri Krishna Balaram Mandir, is one of the major ISKCON temples in the world.It is a Gaudiya Vaishnava temple located in the city of Vrindavan, Mathura district, in the Indian state of Uttar Pradesh.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+              <img src=".//images/VAISHNO-DEVI.jpg" alt="">  
+            </div>
+
+            <div class="content">
+                <h3>Vaishno Devi</h3>
+                <p>The Vaishno Devi Temple is an important Hindu temple dedicated to Vaishno Devi located in Katra at the Trikuta Mountains within the Indian Union territory of Jammu and Kashmir.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+              <img src=".//images/Akshardham-Temple-Delhi.jpg" alt="">  
+            </div>
+
+            <div class="content">
+                <h3>Akshardham Temple</h3>
+                <p>Akshardham Temple is a Hindu temple, and spiritual-cultural campus in Delhi, India. The complex displays millennia of traditional and modern Hindu culture, spirituality, and architecture.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+<!-- package section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section -->
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+        <h3> Quick Links </h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+        <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+        <a href="book.php"> <i class="fas fa-angle-right"></i> Book</a>
+        </div>
+
+        <div class="box">
+        <h3> Extra Links </h3>
+        <a href="#"> <i class="fas fa-angle-right"></i>Ask Questions</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>About Us</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>Privacy Policy</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>Terms Of Conditions</a>
+        </div>
+
+        <div class="box">
+        <h3> Contact Details </h3>
+        <a href="#"> <i class="fas fa-phone"></i> +987-456-3210 </a>
+        <a href="#"> <i class="fas fa-phone"></i> +951-230-7846 </a>
+        <a href="mailto: ankursingh759@gmail.com"> <i class="fas fa-envelope"></i> ankursingh759@gmail.com </a>
+        <a href="#"> <i class="fas fa-map"></i> Greater Noida, UP, India, 201308 </a>
+        </div>
+        <div class="box">
+            <h3>Follow Us</h3>
+            <a href="https://www.linkedin.com/in/ankur-singh-541b99255/" target="_blank" > <i class="fab fa-linkedin"></i> LinkedIn </a>
+            <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+            <a href="#"> <i class="fab fa-facebook"></i> Facebook </a>
+            <a href="https://github.com/Indominous1" target="_blank" > <i class="fab fa-github"></i> Github </a>
+        </div>
+    </div>
+
+    <div class="credit">Created by <span>Ankur Singh</span> <div class="far fa-copyright"></div> all rights reserved!
+    </div>
+</section>
+<!-- footer ends -->
+
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="script.js"></script>
+</body>
+</html>
