26b069bdc51fa0e64c290e8c29f0a83e3e9f4de9
Indominous <93007035+Indominous1@users.noreply.github.com>
2023-03-14 19:41:20 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+
+    <!-- custom swiper file link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!-- header starts -->
+<section class="header">
+    <a href="home.php" class="logo">TravelBliss</a>
+
+    <nav class="navbar">
+        <a href="home.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="package.php">package</a>
+        <a href="book.php">Book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+<!-- header ends -->
+
+<div class="heading" style="background:url(.//images/location_pic-1.jpg) no-repeat">
+   <h1>about us</h1>
+</div>
+
+<!-- about section starts -->
+<section class="about">
+    <div class="image">
+        <img src=".//images/image_is_image.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>Why Choose us?</h3>
+        <p>We are a main online travel organization in India giving the best as far as a class can tell with the objective to be  india's Travel Planner.</p>
+        <p>we have the best prices for tours, and a huge selection of destinations will not leave indifferent even avid tourist. We work with the best tour operators who over the years have proven their professionalism. </p>
+        <p>Contacting us, you get full support, from choosing the tour and ending with advice on what to take with you on a trip</p>
+
+        <div class="icon-container">
+            <div class="icons">
+                <i class="fas fa-map"></i>
+                <span>top destinations </span>
+            </div>
+
+            <div class="icons">
+                <i class="fas fa-hand-holding-usd"></i>
+                <span>affordable prices</span>
+            </div>
+
+            <div class="icons">
+                <i class="fas fa-headset"></i>
+                <span>24/7 guide services</span>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- about section ends -->
+
+<section class="reviews">
+
+     <h1 class="heading-title">client reviews </h1>
+
+    <div class="swiper reviews-slider">
+
+        <div class="swiper-wrapper">
+
+        <div class="swiper-slide slide">
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <p>Fantastic!!! this was our second holiday in 4 months that I booked with TravelBliss all went smoothly.Thank you TravelBliss for your great service</p>
+            <h3>Sunil Pawar</h3>
+            <span>traveler</span>
+            <img src=".//images/traveller 1.jpg" alt="">
+        </div>
+    
+        <div class="swiper-slide slide">
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <p>We were so pleased with our Kedarnath trip that you organised. great service and what we expected is exactly what we received. We will be holiday inn again shortly and my first stop will be the TravelBliss. <br> Thank You</p>
+            <h3>Rehana</h3>
+            <span>traveler</span>
+            <img src=".//images/traveller 2.jpg" alt="">
+        </div>
+
+        <div class="swiper-slide slide">
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <p>My family finished Char Dham Yatra successfully last week. Everything went very well and I would like to pass on special THANKS to Surendra who was exceptionally humble in whole tour. Thank to all of you.</p>
+            <h3>Sarvottam Shenvi</h3>
+            <span>traveler</span>
+            <img src=".//images/traveller 3.jpg" alt="">
+        </div>
+
+        <div class="swiper-slide slide">
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <p>We have had an excellent trip to Kashi vishvnath temple. It was one of our best trip in recent times. This was our first trip with "TravelBliss" and we all, especially my parents, are very happy with the way it was organized.<br>I would like to thank "TravelBliss" for organizing this trip for us very efficiently.
+            </p>
+            <h3>Parikshit Rawat</h3>
+            <span>traveler</span>
+            <img src=".//images/traveller 4.jpg" alt="">
+        </div>
+
+        <div class="swiper-slide slide">
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <p>Awesome booking guys. Everything was perfect. Will recommend you to anyone looking for a holiday.Gave us all the information we needed and everything was organised when we arrived. Excellent trip. Will be booking through TravelBliss again!</p>
+            <h3>Suman Gupta</h3>
+            <span>traveler</span>
+            <img src=".//images/traveller 5.jpg" alt="">
+        </div>
+
+        <div class="swiper-slide slide">
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <p>I want to thank you, and anyone who helped you, with your excellent service that you gave us. Our stay at Vaishno Devi was outstanding.The hotels which was recommended to us also are good The whole trip worked very well. Details of the offer very clear with accommodation options spelt out clearly when booking Booking were Fairly straightforward and the payment was secure.<br>Thank you once again TravelBliss</p>
+            <h3>Aditi Kenjalkar</h3>
+            <span>traveler</span>
+            <img src=".//images/traveller 6.jpg" alt="">
+        </div>
+
+      </div>
+
+    </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section -->
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+        <h3> Quick Links </h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+        <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+        <a href="book.php"> <i class="fas fa-angle-right"></i> Book</a>
+        </div>
+
+        <div class="box">
+        <h3> Extra Links </h3>
+        <a href="#"> <i class="fas fa-angle-right"></i>Ask Questions</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>About Us</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>Privacy Policy</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>Terms Of Conditions</a>
+        </div>
+
+        <div class="box">
+        <h3> Contact Details </h3>
+        <a href="#"> <i class="fas fa-phone"></i> +987-456-3210 </a>
+        <a href="#"> <i class="fas fa-phone"></i> +951-230-7846 </a>
+        <a href="mailto: ankursingh759@gmail.com"> <i class="fas fa-envelope"></i> ankursingh759@gmail.com </a>
+        <a href="#"> <i class="fas fa-map"></i> Greater Noida, UP, India, 201308 </a>
+        </div>
+        <div class="box">
+            <h3>Follow Us</h3>
+            <a href="https://www.linkedin.com/in/ankur-singh-541b99255/" target="_blank" > <i class="fab fa-linkedin"></i> LinkedIn </a>
+            <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+            <a href="#"> <i class="fab fa-facebook"></i> Facebook </a>
+            <a href="https://github.com/Indominous1" target="_blank" > <i class="fab fa-github"></i> Github </a>
+        </div>
+    </div>
+
+    <div class="credit">Created by <span>Ankur Singh</span> <div class="far fa-copyright"></div> all rights reserved!
+    </div>
+</section>
+<!-- footer ends -->
+
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="script.js"></script>
+</body>
+</html>
